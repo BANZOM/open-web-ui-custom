ec0d70d4ad00b21776e120ab2659101f8cee7f34
Aditya Panwar <aditya.panwar.2208@gmail.com>
2025-04-06 19:33:50 +0530

    update startup script to include additional container run commands and add execution warning

--- startup.sh
+++ startup.sh
@@ -1,3 +1,6 @@
+#### This is my dump of the startup script for the webui container
+### DO NOT EXECUTE THIS SCRIPT
+
 docker run -itd \
   --gpus all \
   -v banzo_ollama_data:/data/ollama \
@@ -5,3 +8,11 @@ docker run -itd \
   -e TUNNEL_TOKEN="YOUR_TUNNEL_TOKEN" \
   --name banzo-webui-container \
   my-webui-app-image:latest 
+
+
+docker run -d -p 3000:8080 --gpus=all -v ollama:/root/.ollama -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:ollama
+
+docker run -itd --gpus all \
+  --name banzo-webui-debug-container \
+  --restart always \
+  banzo-openwebui-debug:3.0 
