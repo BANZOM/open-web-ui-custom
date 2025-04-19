35a0188ffa81e3f1b509fc6e808278bac650a418
Aditya Panwar <aditya.panwar.2208@gmail.com>
2025-04-19 13:47:08 +0530

    add build context to Docker Compose and improve Cloudflared tunnel startup validation

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,10 +8,17 @@ if [ -z "$CLOUDFLARED_TUNNEL_TOKEN" ]; then
 fi
 
 echo "Starting Cloudflared tunnel..."
-cloudflared tunnel --no-autoupdate run --token "$CLOUDFLARED_TUNNEL_TOKEN" >> /app/backend/cloudflared.logs 2&>1 &
+cloudflared tunnel --no-autoupdate run --token "$CLOUDFLARED_TUNNEL_TOKEN" >> /app/backend/cloudflared.logs 2>&1 &
+CLOUDFLARED_PID=$!
+sleep 5
+if  ! kill -0 "$CLOUDFLARED_PID"; then
+    echo "Error: Cloudflared tunnel failed to start."
+    exit 1
+fi
 echo "Cloudflared tunnel started."
 
 if [ -f "/app/backend/start.sh" ]; then
+    echo "Starting application..."
     /bin/bash /app/backend/start.sh
 else
     echo "Error: /app/backend/start.sh not found in base image."
